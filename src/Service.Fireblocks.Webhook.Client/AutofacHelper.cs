346c9816e8da2450654198e6392d8419edd76e45
Aleksandr B <aleksandr.b@smplt.net>
2022-01-19 11:53:31 +0400

    added lib

---
+++ src/Service.Fireblocks.Webhook.Client/AutofacHelper.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using MyJetWallet.Sdk.NoSql;
+using MyNoSqlServer.DataReader;
+using Service.Blockchain.Wallets.MyNoSql.Addresses;
+
+namespace Service.Fireblocks.Webhook.Client
+{
+    public static class AutofacHelper
+    {
+        public static void RegisterFireblocksWebhookCache(this ContainerBuilder builder, MyNoSqlTcpClient myNoSqlTcpClient)
+        {
+            builder.RegisterMyNoSqlReader<VaultAssetNoSql>(myNoSqlTcpClient, VaultAssetNoSql.TableName);
+        }
+    }
+}
